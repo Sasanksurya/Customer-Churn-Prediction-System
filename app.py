e2eaeb51bc1d796596ab6d601a934d530b2348e1
Sasank Surya <shashanksurya24@gmail.com>
2026-05-07 01:52:12 +0530

    Initial customer churn prediction project

---
+++ app.py
@@ -0,0 +1,101 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load model
+model = joblib.load("models/best_rf_model.pkl")
+
+# App title
+st.title("Customer Churn Prediction System")
+
+st.write("Enter customer details below:")
+
+# User Inputs
+gender = st.selectbox(
+    "Gender",
+    ["Male", "Female"]
+)
+
+senior_citizen = st.selectbox(
+    "Senior Citizen",
+    [0, 1]
+)
+
+partner = st.selectbox(
+    "Partner",
+    ["Yes", "No"]
+)
+
+dependents = st.selectbox(
+    "Dependents",
+    ["Yes", "No"]
+)
+
+tenure = st.slider(
+    "Tenure Months",
+    1,
+    72
+)
+
+monthly_charges = st.number_input(
+    "Monthly Charges",
+    min_value=0.0
+)
+
+contract = st.selectbox(
+    "Contract",
+    [
+        "Month-to-month",
+        "One year",
+        "Two year"
+    ]
+)
+
+internet_service = st.selectbox(
+    "Internet Service",
+    [
+        "DSL",
+        "Fiber optic",
+        "No"
+    ]
+)
+
+payment_method = st.selectbox(
+    "Payment Method",
+    [
+        "Electronic check",
+        "Mailed check",
+        "Bank transfer (automatic)",
+        "Credit card (automatic)"
+    ]
+)
+
+# Create input dataframe
+input_data = pd.DataFrame({
+    "Gender": [gender],
+    "Senior Citizen": [senior_citizen],
+    "Partner": [partner],
+    "Dependents": [dependents],
+    "Tenure Months": [tenure],
+    "Monthly Charges": [monthly_charges],
+    "Contract": [contract],
+    "Internet Service": [internet_service],
+    "Payment Method": [payment_method]
+})
+
+# Prediction button
+if st.button("Predict Churn"):
+
+    prediction = model.predict(input_data)
+
+    probability = model.predict_proba(input_data)[0][1]
+
+    if prediction[0] == 1:
+        st.error(
+            f"Customer is likely to churn.\nProbability: {probability:.2f}"
+        )
+
+    else:
+        st.success(
+            f"Customer is likely to stay.\nProbability: {probability:.2f}"
+        )
